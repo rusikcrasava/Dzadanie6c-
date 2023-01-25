f163059c9d69ad6bc1b29f98d2f8d5bbf5fe2e7c
rusikcrasava <rusikcrasava@mail.ru>
2023-01-25 13:05:08 +0300

    Вторая задача выполнена

---
+++ zada4a2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double numberX = (b2 - b1)/(k1 - k2);
+double numberY = k2 * numberX + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых: {numberX},{numberY}");
